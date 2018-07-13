1235d04a0b2e2cfa89e4c621caae74c4a7ffa84a
guomin <yushi5344@gmail.com>
2018-07-13 19:40:34 +0800

    查询构造器 QueryBuilder

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use common\models\Poststatus;
+use common\models\Adminuser;
 use yii\helpers\ArrayHelper;
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
@@ -23,8 +24,20 @@ use yii\helpers\ArrayHelper;
 //        $psObjs=Poststatus::find()->all();
 //        $allStatus=ArrayHelper::map($psObjs,'id','name');
         //下拉选择方法2
-        $psArr=Yii::$app->db->createCommand("SELECT id,name from poststatus ")->queryAll();
-        $allStatus=ArrayHelper::map($psArr,'id','name');
+//        $psArr=Yii::$app->db->createCommand("SELECT id,name from poststatus ")->queryAll();
+//        $allStatus=ArrayHelper::map($psArr,'id','name');
+        //下拉选择方法3
+//        $allStatus=(new \yii\db\Query())
+//        ->select(['name','id'])
+//        ->from('poststatus')
+//        ->indexBy('id')
+//        ->column();
+        //下拉选择方法4
+        $allStatus=Poststatus::find()
+        ->select(['name','id'])
+        ->orderBy('position')
+        ->indexBy('id')
+        ->column();
     ?>
     <?=
         $form->field($model, 'status')
@@ -35,7 +48,17 @@ use yii\helpers\ArrayHelper;
 
     <?= $form->field($model, 'update_time')->textInput() ?>
 
-    <?= $form->field($model, 'author_id')->textInput() ?>
+    <?=
+        $form->field($model, 'author_id')
+        ->dropDownList(
+                Adminuser::find()
+                ->select(['nickname','id'])
+                ->orderBy('id')
+                ->indexBy('id')
+                ->column(),
+            ['prompt'=>'请选择作者']);
+
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? '新增' : '修改', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
