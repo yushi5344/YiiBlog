847b5015386b75e497e69dfd69cebc4e9d802cfb
guomin <yushi5344@gmail.com>
2018-07-13 19:21:02 +0800

    form表单下拉修改

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -2,7 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use common\models\Poststatus;
+use yii\helpers\ArrayHelper;
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
 /* @var $form yii\widgets\ActiveForm */
@@ -17,8 +18,18 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'tags')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?php
+        //下拉选择方法1
+//        $psObjs=Poststatus::find()->all();
+//        $allStatus=ArrayHelper::map($psObjs,'id','name');
+        //下拉选择方法2
+        $psArr=Yii::$app->db->createCommand("SELECT id,name from poststatus ")->queryAll();
+        $allStatus=ArrayHelper::map($psArr,'id','name');
+    ?>
+    <?=
+        $form->field($model, 'status')
+        ->dropDownList($allStatus,['prompt'=>'请选择状态']);
+    ?>
 
     <?= $form->field($model, 'create_time')->textInput() ?>
 
@@ -27,7 +38,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'author_id')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? '新增' : '修改', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
